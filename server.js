1a74dea3a929d04326243e2669fce9e6daa75946
John Hooper <jwhooperjr@gmail.com>
2020-04-09 06:50:45 -0600

    Include endpoints for stubbed routes

--- server.js
+++ server.js
@@ -6,42 +6,42 @@ const QADB = require('.db/QADB');
 const port = 3000;
 
 // List Questions
-app.get('/', (req, res) => {
+app.get('/qa/:product_id', (req, res) => {
 
 });
 
 // Add a Question
-app.post('/', (req, res) => {
+app.post('/qa/:product_id', (req, res) => {
 
 });
 
 // Mark Question as Helpful
-app.put('/', (req, res) => {
+app.put('/qa/question/:question_id/helpful', (req, res) => {
 
 });
 
 // Report Question
-app.put('/', (req, res) => {
+app.put('/qa/question/:question_id/report', (req, res) => {
 
 });
 
 // List Answers
-app.get('/answers', (req, res) => {
+app.get('/qa/:question_id/answers', (req, res) => {
     
 });
 
 // Add an Answer
-app.post('/', (req, res) => {
+app.post('/qa/:question_id/answers', (req, res) => {
 
 });
 
 // Mark Answer as Helpful
-app.put('/', (req, res) => {
+app.put('/qa/answer/:answer_id/helpful', (req, res) => {
 
 });
 
 // Report Answer
-app.put('/' (req, res) => {
+app.put('/qa/answer/:answer_id/report' (req, res) => {
     
 });
 
